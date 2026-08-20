5c7172911eea0f5ec8176e66b9ea0f786475671b
Minh Triet Tran <minhtriet.01102007@gmail.com>
2026-08-20 09:44:10 +0700

    Add Complex class with basic operations

--- pr_0101/complex.h
+++ pr_0101/complex.h
@@ -1,1 +1,11 @@
-
+class Complex{
+    private:
+        double realPart;
+        double imaginaryPart;
+        void setComplexNumber(double r, double i);
+    public:
+        Complex(double r = 0.0 , double i = 0.0);
+        Complex addition(const Complex& a);
+        Complex subtraction(const Complex& a);
+        void printComplex();
+};
