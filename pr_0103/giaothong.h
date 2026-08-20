3ffe650c800134a144e293e54da1fb2d8e79fde1
Minh Triet Tran <minhtriet.01102007@gmail.com>
2026-08-20 11:41:28 +0700

    Add PhuongTienGiaoThong class definition

--- pr_0103/giaothong.h
+++ pr_0103/giaothong.h
@@ -1,1 +1,17 @@
+#include <string>
 
+class PhuongTienGiaoThong{
+    private:
+        std::string hang_sx;
+        std::string ten_phuongtien;
+        int nam_sx;
+        float v_max;
+    public:
+        PhuongTienGiaoThong();
+        PhuongTienGiaoThong(std::string hang_sx, std::string ten_phuongtien,
+                            int nam_sx, float v_max);
+        virtual ~PhuongTienGiaoThong();
+        virtual void nhap();
+        virtual void xuat() const;
+        float getVmax() const;
+};
