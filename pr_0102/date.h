419f2b06ef89617bbcff72316f18efd728ed8f38
Minh Triet Tran <minhtriet.01102007@gmail.com>
2026-08-20 09:45:38 +0700

    Add Date class with basic functionality

--- pr_0102/date.h
+++ pr_0102/date.h
@@ -1,1 +1,18 @@
-
+class Date{
+    private:
+        int day;
+        int month;
+        int year;
+    public:
+        Date(int d=1, int m=1, int y=2003);
+        int getDay();
+        int getMonth();
+        int getYear();
+        void setDay(int d);
+        void setMonth(int m);
+        void setYear(int y);
+        bool isLeapYear(int y);
+        int monthDays();
+        void nextDay();
+        void print();
+};
