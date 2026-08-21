b1dca45094a774294ec06d8171c7656d94a48ab5
Minh Triet Tran <minhtriet.01102007@gmail.com>
2026-08-21 08:36:34 +0700

    Update nguoi.h

--- pr_0201/nguoi.h
+++ pr_0201/nguoi.h
@@ -1,1 +1,15 @@
+#ifndef NGUOI_H
+#define NGUOI_H
+#include <string>
 
+class Nguoi{
+    private:
+        std::string ma_dinh_danh;
+        std::string ho_ten;
+    public:
+        Nguoi();
+        Nguoi(std::string ma_dinh_danh, std::string ho_ten);
+        void nhap();
+        void xuat() const;
+};
+#endif
