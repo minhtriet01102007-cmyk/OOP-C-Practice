b7ab96052725601a620b6b66494dd3482e3be824
Minh Triet Tran <minhtriet.01102007@gmail.com>
2026-08-22 16:38:10 +0700

    Add Date class definition in date.h

--- pr_0202/date.h
+++ pr_0202/date.h
@@ -1,1 +1,12 @@
-
+#pragma one
+class Date{
+    private:
+        int day;
+        int month;
+        int year;
+    public:
+        Date();
+        Date(int day, int month, int year);
+        void nhap();
+        void xuat() const;
+};
