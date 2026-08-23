28e8aa9858ae8ba630d53ba6b34bf4572f19e757
Minh Triet Tran <minhtriet.01102007@gmail.com>
2026-08-23 13:43:18 +0700

    Update hanghoa.hpp

--- pr_0301/hanghoa.hpp
+++ pr_0301/hanghoa.hpp
@@ -1,1 +1,14 @@
+#pragma once
+#include <string>
+using namespace std;
 
+class Hanghoa{
+    private:
+        string ma_hang;
+        string ten_hang;
+    public:
+        Hanghoa();
+        Hanghoa(string ma_hang, string ten_hang);
+        void nhap();
+        void xuat() const;
+};
