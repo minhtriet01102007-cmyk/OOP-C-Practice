977a69842905e4f15f8ceb7f513db2a584b926b7
Minh Triet Tran <minhtriet.01102007@gmail.com>
2026-08-24 19:20:58 +0700

    Add files via upload

---
+++ pr_0401/sach.cpp
@@ -0,0 +1,9 @@
+#include "sach.h"
+#include <iostream>
+#include <string>
+
+Sach::Sach() : ma_sach(""), ten_sach("")
+{}
+Sach::Sach(std::string ma_sach, std::string ten_sach)
+    : ma_sach(ma_sach), ten_sach(ten_sach)
+{}
