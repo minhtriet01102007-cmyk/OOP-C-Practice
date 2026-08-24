9244420f4ca3f88089797aac682f36925c9d3657
Minh Triet Tran <minhtriet.01102007@gmail.com>
2026-08-24 18:59:35 +0700

    Update sach.h

--- pr_0401/sach.h
+++ pr_0401/sach.h
@@ -1,1 +1,13 @@
+#pragma once
+#include <string>
 
+class Sach{
+    private:
+        std::string ma_sach;
+        std::string ten_sach;
+    public:
+        Sach();
+        Sach(std::string ma_sach, std::string ten_sach);
+        void nhap();
+        void xuat() const;
+};
