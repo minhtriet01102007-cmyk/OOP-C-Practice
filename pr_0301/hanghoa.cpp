8c80327a8b42ac38b053f4aeb2836f5c1818e638
Minh Triet Tran <minhtriet.01102007@gmail.com>
2026-08-24 18:23:49 +0700

    Update hanghoa.cpp

--- pr_0301/hanghoa.cpp
+++ pr_0301/hanghoa.cpp
@@ -1,20 +1,36 @@
 #include "hanghoa.hpp"
-#include <string>
 #include <iostream>
+
 using namespace std;
 
-Hanghoa::Hanghoa() : ma_hang(""), ten_hang("")
-{}
-Hanghoa::Hanghoa(string ma_hang, string ten_hang)
-       : ma_hang(ma_hang), ten_hang(ten_hang)
-{}
-void Hanghoa::nhap(){
-    cout << "Nhap ma hang: ";
-    getline(cin, ma_hang);
-    cout << "Nhap ten hang: ";
-    getline(cin, ten_hang);
+Hanghoa::Hanghoa() {
+    maHang = "";
+    tenHang = "";
 }
-void Hanghoa::xuat() const{
-    cout << "Ma hang: " << ma_hang << endl;
-    cout << "Ten hangL " << ten_hang << endl;
+
+Hanghoa::Hanghoa(string maHang, string tenHang) {
+    this->maHang = maHang;
+    this->tenHang = tenHang;
+}
+
+void Hanghoa::nhap() {
+    cout << "Nhap ma hang: ";
+    cin >> maHang;
+
+    cin.ignore();
+    cout << "Nhap ten hang: ";
+    getline(cin, tenHang);
+}
+
+void Hanghoa::xuat() const {
+    cout << "Ma hang: " << maHang << endl;
+    cout << "Ten hang: " << tenHang << endl;
+}
+
+string Hanghoa::getMaHang() const {
+    return maHang;
+}
+
+string Hanghoa::getTenHang() const {
+    return tenHang;
 }
