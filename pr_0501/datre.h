6730766e493eceeec891817f8778db0d68df3d37
Minh Triet Tran <minhtriet.01102007@gmail.com>
2026-08-25 07:31:38 +0700

    Create datre.h

---
+++ pr_0501/datre.h
@@ -0,0 +1,12 @@
+#pragma once
+class Date{
+    private:
+        int day;
+        int month;
+        int year;
+    public:
+        Date();
+        Date(int day, int month, int year);
+        void nhap();
+        void xuat() const;
+};
