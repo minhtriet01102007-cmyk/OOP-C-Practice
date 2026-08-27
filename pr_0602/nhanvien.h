9f6dc3ef5b72d72aec3ac17536118806576126c1
Minh Triet Tran <minhtriet.01102007@gmail.com>
2026-08-27 14:20:43 +0700

    Update nhanvien.h

--- pr_0602/nhanvien.h
+++ pr_0602/nhanvien.h
@@ -1,1 +1,15 @@
+#pragma once
+#include <string>
 
+class NhanVien{
+    private:
+        std::string manv;
+        std::string hoten;
+        int tuoi;
+    public:
+        NhanVien();
+        NhanVien(std::string manv, std::string hoten, int tuoi);
+        void Nhap();
+        void Xuat() const;
+        bool VeHuu() const;
+};
