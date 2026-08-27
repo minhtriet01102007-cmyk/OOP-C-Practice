3eb865c7f9780b2de7475daff36bc4e87e0d9d1c
Minh Triet Tran <minhtriet.01102007@gmail.com>
2026-08-27 13:51:02 +0700

    Update dichvu.h

--- pr_0601/dichvu.h
+++ pr_0601/dichvu.h
@@ -1,1 +1,16 @@
+#pragma once
+#include <string>
+
+class DichVu{
+    private:
+        std::string maDV;
+        std::string tenDV;
+        double gia_cuoc;
+    public:
+        DichVu();
+        DichVu(std::string maDV, std::string tenDV, double gia_cuoc);
+        void nhap();
+        void xuat() const;
+        double LayGiacuoc() const;
+};
 
