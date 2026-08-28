05fb74f289cf7daf3b8a3323419a07b8775aa575
Minh Triet Tran <minhtriet.01102007@gmail.com>
2026-08-28 08:18:09 +0700

    Add files via upload

---
+++ pr_0701/monhoc.cpp
@@ -0,0 +1,42 @@
+#include "monhoc.h"
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+MonHoc::MonHoc(){
+    MaMH = "";
+    TenMH = "";
+    DTB = 0;
+    soTC = 0;
+}
+MonHoc::MonHoc(string MaMH, string TenMH, double DTB, int soTC){
+    this->MaMH = MaMH;
+    this->TenMH = TenMH;
+    this->DTB = DTB;
+    this->soTC = soTC;
+}
+MonHoc::~MonHoc(){}
+
+void MonHoc::nhap(){
+    cout << "Nhap ma mon hoc: ";
+    getline(cin, MaMH);
+    cout << "Nhap ten mon hoc: ";
+    getline(cin, TenMH);
+    cout << "Nhap diem trung binh: ";
+    cin >> DTB;
+    cout << "Nhap so tin chi: ";
+    cin >> soTC;
+    cin.ignore();
+}
+void MonHoc::xuat() const{
+    cout << "Ma mon hoc: " << MaMH << endl;
+    cout << "Ten mon hoc: " << TenMH << endl;
+    cout << "Diem trung binh: " << fixed << setprecision(2) << DTB << endl;
+    cout << "So tin chi: " << soTC << endl;
+}
+int MonHoc::getTC() const{
+    return soTC;
+}
+void MonHoc::setTC(int soTC){
+    this->soTC = soTC;
+}
