4d3a66e54bcf282c2a5ae7203a52396e8b4ef0ac
Minh Triet Tran <minhtriet.01102007@gmail.com>
2026-08-28 08:12:57 +0700

    Add MonHoc class definition with member functions

--- pr_0701/monhoc.h
+++ pr_0701/monhoc.h
@@ -1,1 +1,18 @@
+#pragma once
+#include <string>
 
+class MonHoc{
+    private:
+        std::string MaMH;
+        std::string TenMH;
+        double DTB;
+        int soTC;
+    public:
+        MonHoc();
+        MonHoc(std::string MaMH, std::string TenMH, double DTB, int soTC);
+        ~MonHoc();
+        void nhap();
+        void xuat() const;
+        int getTC() const;
+        void setTC(int soTC);
+};
