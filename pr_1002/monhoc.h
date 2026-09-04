71f9039e5c5a8352a26c70817e363bdb3e9c4507
Minh Triet Tran <minhtriet.01102007@gmail.com>
2026-09-04 13:13:02 +0700

    Add MONHOC class definition
    Define MONHOC class with member variables and methods.

--- pr_1002/monhoc.h
+++ pr_1002/monhoc.h
@@ -1,1 +1,13 @@
+#pragma once
+#include <string>
 
+class MONHOC{
+    private:
+        std::string maMH;
+        std::string tenMH;
+    public:
+        MONHOC();
+        MONHOC(std::string maMH, std::string tenMH);
+        void nhap();
+        void xuat() const;
+};
