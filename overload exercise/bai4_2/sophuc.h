edc9f27dd7e835b0fe377609b924deae47bfffdf
Minh Triet Tran <minhtriet.01102007@gmail.com>
2026-09-24 15:51:16 +0700

    Update sophuc.h

--- overload exercise/bai4_2/sophuc.h
+++ overload exercise/bai4_2/sophuc.h
@@ -1,1 +1,38 @@
+#pragma once
+#include <iostream>
+using namespace std;
 
+class SoPhuc{
+    private:
+        double thuc;
+        double ao;
+    public:
+        SoPhuc();
+        SoPhuc(double thuc, double ao);
+        SoPhuc(double x);
+        SoPhuc(const SoPhuc& sp);
+
+        SoPhuc operator+(const SoPhuc& sp);
+        SoPhuc operator-(const SoPhuc& sp);
+        SoPhuc operator*(const SoPhuc& ps);
+        SoPhuc operator/(const SoPhuc& ps);
+        SoPhuc& operator=(const SoPhuc& ps);
+        SoPhuc& operator+=(const SoPhuc& ps);
+        SoPhuc& operator-=(const SoPhuc& ps);
+
+        bool operator>(const SoPhuc& ps);
+        bool operator<(const SoPhuc& ps);
+        bool operator==(const SoPhuc& ps);
+        bool operator>=(const SoPhuc& ps);
+        bool operator<=(const SoPhuc& ps);
+        bool operator!=(const SoPhuc& ps);
+
+        SoPhuc& operator++();
+        SoPhuc& operator--();
+
+        operator float();
+        operator int();
+
+        friend istream& operator>>(istream& in, SoPhuc& ps);
+        friend ostream& operator<<(ostream& out, const SoPhuc& ps);
+};  
