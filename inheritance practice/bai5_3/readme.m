197a4b543c1f7dff356c5289fc0c16d53a0a61f7
Minh Triet Tran <minhtriet.01102007@gmail.com>
2026-09-25 08:11:01 +0700

    Update readme.m for clarity and formatting
    Formatted the text for better readability and corrected spacing.

---
+++ inheritance practice/bai5_3/readme.m
@@ -0,0 +1,9 @@
+Một chiếc xe máy chạy 100km tốn 2 lit xăng, cứ chở thêm 10kg hàng xe tốn thêm 0.1 lit xăng.
+Một chiếc xe tải chạy 100km tốn 20 lit xăng, cứ chở thêm 1000kg hàng xe tốn thêm 1lit xăng.
+Dùng kế thừa xây dựng lớp XeMay và XeTai cho phép:
+- Chất một lượng hàng lên xe.
+- Bỏ bớt một lượng hàng xuống xe.
+- Đổ một lượng xăng vào xe.
+- Cho xe chạy một đoạn đường.
+- Kiểm tra xem xe đã hết xăng chưa.
+- Cho biết lượng xăng còn trong xe
