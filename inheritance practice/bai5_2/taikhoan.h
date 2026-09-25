5a2be4f71f8a4491ccb68dbf73d5947890bdadb2
Minh Triet Tran <minhtriet.01102007@gmail.com>
2026-09-25 11:06:43 +0700

    Implement TaiKhoan class with balance management

--- inheritance practice/bai5_2/taikhoan.h
+++ inheritance practice/bai5_2/taikhoan.h
@@ -1,1 +1,16 @@
+#pragma once
 
+class TaiKhoan{
+    private:
+        float m_fSoDu = 0;
+    public:
+        float baoSoDu(){
+        return m_fSoDu;
+        }
+        void napTien(float fSoTien){
+        m_fSoDu += fSoTien;
+        }
+        void rutTien(float fSoTien){
+            if (fSoTien <= m_fSoDu) m_fSoDu -= fSoTien;
+        }
+};
